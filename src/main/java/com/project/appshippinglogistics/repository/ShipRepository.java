6347f8f08f73d95b7c2646a89f396b814d100811
andres.mera <feli041089@gmail.com>
2024-03-01 07:13:53 -0500

    feature/vehiship : Nuevo endpoint para las consultas por number del barco.

--- src/main/java/com/project/appshippinglogistics/repository/ShipRepository.java
+++ src/main/java/com/project/appshippinglogistics/repository/ShipRepository.java
@@ -2,8 +2,14 @@ package com.project.appshippinglogistics.repository;
 
 import com.project.appshippinglogistics.model.Ship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShipRepository extends JpaRepository<Ship, Integer> {
+
+    @Query("select s from Ship s where s.fleetNumber like %:fleetNumber%")
+    List<Ship> getShipList(String fleetNumber);
 }
